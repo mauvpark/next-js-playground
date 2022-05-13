2ed469d071e5704b898c1c7864d801a32830071a
mauvpark <mauvpark@gmail.com>
2022-05-13 16:34:27 +0900

    수정: 테스트 코드 수정

--- __tests__/pages/api/fetch-data.test.js
+++ __tests__/pages/api/fetch-data.test.js
@@ -11,32 +11,6 @@ test("Returns data from posts db", async () => {
 			body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
 		},
 	]);
-
-	expect(await fetchData(userListData(), 0)).toStrictEqual([
-		{
-			id: 1,
-			name: "Leanne Graham",
-			username: "Bret",
-			email: "Sincere@april.biz",
-			address: {
-				street: "Kulas Light",
-				suite: "Apt. 556",
-				city: "Gwenborough",
-				zipcode: "92998-3874",
-				geo: {
-					lat: "-37.3159",
-					lng: "81.1496",
-				},
-			},
-			phone: "1-770-736-8031 x56442",
-			website: "hildegard.org",
-			company: {
-				name: "Romaguera-Crona",
-				catchPhrase: "Multi-layered client-server neural-net",
-				bs: "harness real-time e-markets",
-			},
-		},
-	]);
 });
 
 test("Returns data from user list db", async () => {
